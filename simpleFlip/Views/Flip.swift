b478a4b87acec295fb480cd7bd21cd72298758e1
Aidan <aidanbryant11@gmail.com>
2019-10-12 16:25:52 -0400

    went back to first system of flipping

--- simpleFlip/Views/Flip.swift
+++ simpleFlip/Views/Flip.swift
@@ -34,23 +34,17 @@ var body: some View {
        }
     }
 func startGyro() {
-    motionManager.gyroUpdateInterval = 0.2
-    motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, Error) in
-        if let myData = data {
-            let currTime = DispatchTime.now()
-            let dt = prevTime.distance(to: currTime).milliseconds
-            prevTime = currTime
-            // assuming rotationRate is in degrees/millisecond.
-            dTheta = myData.rotationRate.y * dt
-            // If not milliseconds, convert dt to correct time unit
-            // If rotationRate is in radians, convert to degrees or make future comparison check for 2*pi instead of 360 degrees
-            totalRotation += dTheta
-            if totalRotation > 360 - delta && totalRotation < 360 + delta {
-                tiltOut = tiltOut + 1
-                self.labelText = "\(tiltOut)"
-                totalRotation -= 360
-            }
-        }
-    }
+motionManager.gyroUpdateInterval = 0.2
+motionManager.startGyroUpdates(to: OperationQueue.current!) {
+ (data, Error) in
+
+ if let myData = data
+ {
+   if myData.rotationRate.y > 3
+   {
+      tiltOut = tiltOut + 1
+    self.labelText = "\(tiltOut)"   }
+}
+}
 }
 }
