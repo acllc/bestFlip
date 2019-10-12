67e00af09eb5b34cf9257b218b976f4c7f26d166
Aidan <aidanbryant11@gmail.com>
2019-10-12 00:30:33 -0400

    Initial Commit

---
+++ simpleFlip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simpleFlip
+//
+//  Created by Aidan Bryant on 10/12/19.
+//  Copyright © 2019 Aidan Bryant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
