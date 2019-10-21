00afb3610aac0b3a3710cd6487aafc4c8b6a31b1
Aidan <aidanbryant11@gmail.com>
2019-10-20 23:53:24 -0400

    Just a error when running

--- simpleFlip/Views/Flip.swift
+++ simpleFlip/Views/Flip.swift
@@ -8,6 +8,33 @@
 
 import SwiftUI
 import CoreMotion
+import Foundation
+
+var prevTime = DispatchTime.now()
+var totalRotation: CGFloat = 0
+var delta: CGFloat = 10
+
+extension DispatchTimeInterval {
+    func toDouble() -> Double? {
+        var result: Double? = 0
+
+        switch self {
+        case .seconds(let value):
+            result = Double(value)
+        case .milliseconds(let value):
+            result = Double(value)*0.001
+        case .microseconds(let value):
+            result = Double(value)*0.000001
+        case .nanoseconds(let value):
+            result = Double(value)*0.000000001
+
+        case .never:
+            result = nil
+        }
+
+        return result
+    }
+}
 
 struct Flip: View {
     var body: some View {
@@ -38,10 +65,15 @@ func startGyro() {
     motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, Error) in
         if let myData = data {
             let currTime = DispatchTime.now()
-            let dt = prevTime.distance(to: currTime).millisecond
+            var dt = 0
+            let interval = prevTime.distance(to: currTime)
+            if case .nanoseconds(let value) = interval {
+                dt = value
+            }
             prevTime = currTime
             // assuming rotationRate is in degrees/millisecond.
-            let dTheta = myData.rotationRate.y * dt
+            let dTheta: CGFloat = CGFloat(myData.rotationRate.y) * CGFloat(dt)
+            
             // If not milliseconds, convert dt to correct time unit
             // If rotationRate is in radians, convert to degrees or make future comparison check for 2*pi instead of 360 degrees
             totalRotation += dTheta
@@ -53,4 +85,5 @@ func startGyro() {
         }
     }
 }
+    
 }
